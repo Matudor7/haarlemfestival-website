e2bf7b0885b531534cd0421d7b426926b8eee5ea
Tudor <tdr.nsk@gmail.com>
2023-02-14 15:19:24 +0100

    Create homeController.php

---
+++ app/controllers/homeController.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/controller.php'; 
+
+class HomeController extends Controller{
+    public function index(){
+        require __DIR__ . '/../views/home/index.php';
+    }
+}
+?>

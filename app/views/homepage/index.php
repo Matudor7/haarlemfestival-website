59f6b371916a0a2e92bcf6562cecef12bf224e2d
Andy Rodriguez <577147@student.inholland.nl>
2023-02-15 21:33:18 +0100

    first tries

--- app/views/homepage/index.php
+++ app/views/homepage/index.php
@@ -1,1 +1,22 @@
-Homepage
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Homepage</title>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <link href="/style.css" rel="stylesheet" type="text/css">
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+    </head>
+    <body>
+        <h1>The Festival</h1>
+        <nav class="navbar bg-body-tertiary">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/app/media/NavbarLogo.png" alt="Bootstrap" width="30" height="24">
+    </a>
+  </div>
+</nav>
+
+<img src="NavbarLogo.jpg" alt="Bootstrap" width="30" height="24">
+    </body>

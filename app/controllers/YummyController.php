869559511b9e3d92419b457b2eeba03527c58212
Alessandra Ribeiro <689693@student.inholland.nl>
2023-02-16 23:15:36 +0100

    linked pages to its specific pages.

---
+++ app/controllers/YummyController.php
@@ -0,0 +1,10 @@
+<?php
+require __DIR__ . '/controller.php'; 
+
+class YummyController extends Controller{
+    public function index(){
+        require __DIR__ . '/../views/homepage/YummyView.php';
+    }
+}
+
+?>
---
+++ app/views/homepage/HistoryView.php
@@ -0,0 +1,1 @@
+<h1>Welcome to the history page! </h1>

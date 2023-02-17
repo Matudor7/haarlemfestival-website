536ff18fcd6a93e4ae656fd3bba9d5262fb5bfa4
Tudor <tdr.nsk@gmail.com>
2023-02-17 14:44:45 +0100

    Update homeController.php

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -4,6 +4,12 @@ require __DIR__ . '/../Services/eventService.php';
 
 class HomeController extends Controller{
     public function index(){
+        $eventService = new EventService();
+
+        $danceEvent = $eventService->getByName('dance');
+
+        echo $danceEvent->getUrlRedirect();
+        
         require __DIR__ . '/../views/homepage/index.php';
     }
 }

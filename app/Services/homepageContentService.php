2510d9ffc05fd71a812f00bc7da0574571030291
Tudor <tdr.nsk@gmail.com>
2023-02-17 11:08:44 +0100

    updated service layer

--- app/Services/homepageContentService.php
+++ app/Services/homepageContentService.php
@@ -1,3 +1,12 @@
 <?php
 require __DIR__ . '/../Repositories/homepageContentRepository.php';
+
+class HomepageContentService{
+    public function getAll(){
+        $homepageRepo = new homepageContentRepository();
+        $homepageContents = $homepageRepo->getAll();
+
+        return $homepageContents;
+    }
+}
 ?>

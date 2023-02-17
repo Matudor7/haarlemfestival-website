1394e53afe3d59a8d993eb2835214a3a0232c443
Alessandra Ribeiro <689693@student.inholland.nl>
2023-02-17 10:32:54 +0100

    Moved nag and footer to different pages

--- app/views/homepage/DrTeylersView.php
+++ app/views/homepage/DrTeylersView.php
@@ -1,1 +1,24 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Homepage</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+        <script defer src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+        <link rel="stylesheet" href="/stylesheet.css" type="text/css">
+
+    </head>
+    <body>
+    <?php
+        include __DIR__ . '/nav.php';
+        ?>
+
 <h1>Welcome to the Dr Teylers page! </h1>
+<?php
+        include __DIR__ . '/footer.php';
+        ?>
+    </body>
+</html>

001cd1ede3a529e79d68d8c14aebdd2eace39280
Alessandra Ribeiro <689693@student.inholland.nl>
2023-03-12 17:50:30 +0100

    Update YummyController, patternrouter, and index view

--- app/patternrouter.php
+++ app/patternrouter.php
@@ -60,7 +60,13 @@ class PatternRouter
         // dynamically call relevant controller method
         try {
             $controllerObj = new $controllerName;
-            $controllerObj->{$methodName}();
+            // Pass the restaurant name as a parameter to the detail method
+           /* if ($methodName === 'detail' && !is_null($restaurantName)) {
+                $controllerObj->{$methodName}($restaurantName);
+            } else {*/
+                $controllerObj->{$methodName}();
+           // }
+
         } catch (Exception $e) {
             echo $e;
             http_response_code(404);

4297ad5a25d44737325bc87ff76107b96432cd1c
Matudor7 <tdr.nsk@gmail.com>
2023-03-27 18:48:54 +0200

    Update walkingtourController.php

--- app/controllers/walkingtourController.php
+++ app/controllers/walkingtourController.php
@@ -3,6 +3,8 @@ require __DIR__ . '/controller.php';
 require __DIR__ . '/../Services/eventService.php';
 require __DIR__ . '/../Services/walkingTourService.php';
 require_once __DIR__ . '/../Models/WalkingTourModel.php';
+require __DIR__ . '/../Services/productService.php';
+require __DIR__ . '/../Services/shoppingCartService.php';
 
 class WalkingTourController extends Controller{
     
@@ -19,6 +21,25 @@ class WalkingTourController extends Controller{
         $timetables = $walkingTourService->getTourTimetable();
         $languages = $walkingTourService->getTourLanguages();
 
+        $shoppingCartService = new ShoppingCartService();
+        $shoppingCart = $shoppingCartService->getCartOfUser(1);
+
+        $productService = new ProductService();
+
+        //This causes every object to be its own array
+        $products = [];
+        foreach($shoppingCart->getProducts() as $product_id){
+            array_push($products, $productService->getById($product_id));
+        }
+
+        //This merges all products to be in a single-level array
+        $merged_products = array_merge(...$products);
+
+        //"Amounts" will always be equal to the number of products in the array 
+        $amounts = [];
+        foreach($shoppingCart->getAmount() as $amount){
+            array_push($amounts, $amount);
+        }
         require __DIR__ . '/../views/walkingtour/index.php';
         require __DIR__ .'/../views/buyTicketForm.php';
     }

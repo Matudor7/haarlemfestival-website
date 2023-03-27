834ef8c40038fa6de7a9d97f1c59a1c11f664810
Matudor7 <tdr.nsk@gmail.com>
2023-03-27 18:59:32 +0200

    moved necessary navbar logic into its own file (to be required)

--- app/controllers/homeController.php
+++ app/controllers/homeController.php
@@ -1,35 +1,10 @@
 <?php
 session_start();
 require __DIR__ . '/controller.php';
-require __DIR__ . '/../Services/eventService.php';
-require __DIR__ . '/../Services/productService.php';
-require __DIR__ . '/../Services/shoppingCartService.php';
 
 class HomeController extends Controller{
     public function index(){
-        $eventService = new EventService();
-        $events = $eventService->getAll();
-
-        $shoppingCartService = new ShoppingCartService();
-        $shoppingCart = $shoppingCartService->getCartOfUser(1);
-
-        $productService = new ProductService();
-
-        //This causes every object to be its own array
-        $products = [];
-        foreach($shoppingCart->getProducts() as $product_id){
-            array_push($products, $productService->getById($product_id));
-        }
-
-        //This merges all products to be in a single-level array
-        $merged_products = array_merge(...$products);
-
-        //"Amounts" will always be equal to the number of products in the array 
-        $amounts = [];
-        foreach($shoppingCart->getAmount() as $amount){
-            array_push($amounts, $amount);
-        }
-        
+        require __DIR__ . '/navbarRequirements.php';
         require __DIR__ . '/../views/homepage/index.php';
     }
 }

66f2b9beb8b0edf046b485840a732a2d7c906e27
Matudor7 <tdr.nsk@gmail.com>
2023-04-05 17:30:06 +0200

    Update checkoutController.php

--- app/api/controllers/checkoutController.php
+++ app/api/controllers/checkoutController.php
@@ -1,3 +1,37 @@
 <?php
+require __DIR__ . '/../../Services/paymentService.php';
+require __DIR__ . '/../../Models/paymentModel.php';
 
+class CheckoutController{
+    private $paymentService;
+
+    function __construct(){
+        $this->paymentService = new PaymentService();
+    }
+
+    function index(){
+        if($_SERVER["REQUEST_METHOD"] == "GET"){
+            header('Content-Type: application/json; charset=utf-8');
+            echo json_encode($this->paymentService->getAll());
+        }
+
+        if($_SERVER["REQUEST_METHOD"] == "POST"){
+            $paymentJsonString = file_get_contents('php://input');
+
+            $paymentData = json_decode($paymentJsonString, true);
+
+            $payment = new Payment();
+
+            $payment->setUserid($_SESSION["user_id"]);
+            $payment->setEmail($paymentData["email"]);
+            $payment->setAddress($paymentData["address"]);
+            $payment->setZip($paymentData["zip"]);
+            $payment->setPaymentMethod($paymentData["payment_method"]);
+            $payment->setCardName($paymentData["card_name"]);
+            $payment->setCardNumber($paymentData["card_number"]);
+            $payment->setCardExpiration($paymentData["card_expiration"]);
+            $payment->setCvv($paymentData["CVV"]);
+        }
+    }
+}
 ?>

97bc27518a0e887f4abbf02f217723ec99517c34
Tudor <tdr.nsk@gmail.com>
2023-05-03 12:54:57 +0300

    Update checkoutController.php

--- app/controllers/checkoutController.php
+++ app/controllers/checkoutController.php
@@ -12,12 +12,43 @@ class CheckoutController extends Controller{
         require_once __DIR__ . '/../views/checkout/index.php';
     }
 
+    
     function payment(){
         require_once __DIR__ . '/../Services/paymentService.php';
-        if(isset($_GET["total"]) && isset($_GET["paymentmethod"])){
-            $paymentService = new PaymentService();
-            require_once __DIR__ . '/../views/checkout/payment.php';
+        if(isset($_GET["total"]) && isset($_GET["paymentmethod"])){    
+            $this->paymentProcess();
         }
+    }
+
+    private function paymentProcess(){
+        $paymentService = new PaymentService();
+        require_once __DIR__ . '/../../vendor/autoload.php';
+
+        $mollie = new Mollie\Api\MollieApiClient();
+        $mollie->setApiKey('test_mgqJkkMVNtskk2e9vpgsBhUPsTj9K4');
+
+        $paymentMethod;
+
+        if($_GET["paymentmethod"] == 'ideal'){
+            $paymentMethod = \Mollie\Api\Types\PaymentMethod::IDEAL;
+        } else{
+            $paymentMethod = \Mollie\Api\Types\PaymentMethod::CREDITCARD;
+        }
+
+        $payment = $mollie->payments->create([
+            "amount" => [
+                "currency" => "EUR",
+                "value" => $_GET["total"] . ".00"
+            ],
+            "description" => "Haarlem Festival Payment",
+            "method" => $paymentMethod,
+            "webhookUrl"  => "https://f656-145-81-195-167.ngrok-free.app/checkout/webhook",
+            "redirectUrl" => "https://f656-145-81-195-167.ngrok-free.app/checkout/return",
+        ]);
+
+        $paymentService->addPaymentId($_SESSION['user_id'], $payment->id);
+
+        header("Location: " . $payment->getCheckoutUrl());
     }
 
 

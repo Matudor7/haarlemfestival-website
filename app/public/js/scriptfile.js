6e0d4a65cf2e2660ebb3d8db6318344bb409e1e1
Matudor7 <tdr.nsk@gmail.com>
2023-06-15 17:29:17 +0200

    Update scriptfile.js

--- app/public/js/scriptfile.js
+++ app/public/js/scriptfile.js
@@ -60,71 +60,52 @@ function updateProduct(index, userId, action){
     }
 }
 
-function updateProduct(index, userId, action){
+function updateTotal(userId){
+    const totalPriceHeader = document.getElementById("totalprice");
+
     const shoppingCartUrl = "http://localhost/api/shoppingcart?user_id=" + userId;
-    const amount = document.getElementById("productamount " + index);
-    const div = document.getElementById("productdiv " + index);
-    
-    const productPrice = document.getElementById("productprice " + index);
 
     fetch(shoppingCartUrl)
     .then(response=> response.json())
     .then(data => {
-        return fetch("http://localhost/api/products?product_id=" + data.product_id[index])
-        .then(response=>response.json())
-        .then(product=>{
-            price = product.price * data.amount[index];
-        })
-        .then(()=>{
-            productPrice.innerHTML = '\u20AC' + price;
-            updateTotal(userId);
-        })
+            let totalPrice = 0;
+            const vat = 0.21;
+            for (let i = 0; i < data.product_id.length; i++) {
+                fetch("http://localhost/api/products?product_id=" + data.product_id[i])
+                .then(response=>response.json())
+                .then(product=>{
+                totalPrice += (product.price * data.amount[i]);
+                totalPriceHeader.innerHTML = 'Total: ' + '\u20AC' + (totalPrice + (totalPrice * vat));
+                })
+            }
     })
     .catch(error=> {console.error(error)
     });
-    
-    if(action == "add"){
-        amount.innerHTML = parseInt(amount.innerHTML) + 1;
-    }else if(action == "delete"){
-        if(parseInt(amount.innerHTML) <= 1){
-            location.reload();
-        }else{
-            amount.innerHTML = parseInt(amount.innerHTML) - 1;
-        }
-    }
 }
 
-function addAmount(index, userId, productId) {
-    const apiUrl =
-      'http://localhost/api/shoppingcart?user_id=' +
-      userId +
-      '&product_id=' +
-      productId +
-      '&action=add';
-  
+function addAmount(index, userId, productId){
+    const apiUrl = 'http://localhost/api/shoppingcart?user_id=' + userId + '&product_id=' + productId + '&action=add';
     fetch(apiUrl, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({
-        amount: [{ index: index, increment: 1 }],
-      }),
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            amounts: [{index: index, increment: 1}]
+        })
     })
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Request failed.');
+    .then(response => {
+        if(!response.ok){
+            throw new Error('Request failed.');
         }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        updateProduct(index, userId, 'add');
-      })
-      .catch((error) => {
+    })
+    .then(()=>{
+        updateProduct(index, userId, "add");
+    })
+    .catch(error=> {
         console.error(error);
-      });
-  }
+    });
+}
 
 function removeAmount(index, userId, productId){
     const apiUrl = 'http://localhost/api/shoppingcart?user_id=' + userId + '&product_id=' + productId + '&action=delete';

8794a36f168f6e18492578464a348e2a9be0ebfe
Matudor7 <tdr.nsk@gmail.com>
2023-06-15 17:24:45 +0200

    Update scriptfile.js

--- app/public/js/scriptfile.js
+++ app/public/js/scriptfile.js
@@ -60,27 +60,38 @@ function updateProduct(index, userId, action){
     }
 }
 
-function updateTotal(userId){
-    const totalPriceHeader = document.getElementById("totalprice");
-
+function updateProduct(index, userId, action){
     const shoppingCartUrl = "http://localhost/api/shoppingcart?user_id=" + userId;
+    const amount = document.getElementById("productamount " + index);
+    const div = document.getElementById("productdiv " + index);
+    
+    const productPrice = document.getElementById("productprice " + index);
 
     fetch(shoppingCartUrl)
     .then(response=> response.json())
     .then(data => {
-            let totalPrice = 0;
-            const vat = 0.21;
-            for (let i = 0; i < data.product_id.length; i++) {
-                fetch("http://localhost/api/products?product_id=" + data.product_id[i])
-                .then(response=>response.json())
-                .then(product=>{
-                totalPrice += (product.price * data.amount[i]);
-                totalPriceHeader.innerHTML = 'Total: ' + '\u20AC' + (totalPrice + (totalPrice * vat));
-                })
-            }
+        return fetch("http://localhost/api/products?product_id=" + data.product_id[index])
+        .then(response=>response.json())
+        .then(product=>{
+            price = product.price * data.amount[index];
+        })
+        .then(()=>{
+            productPrice.innerHTML = '\u20AC' + price;
+            updateTotal(userId);
+        })
     })
     .catch(error=> {console.error(error)
     });
+    
+    if(action == "add"){
+        amount.innerHTML = parseInt(amount.innerHTML) + 1;
+    }else if(action == "delete"){
+        if(parseInt(amount.innerHTML) <= 1){
+            location.reload();
+        }else{
+            amount.innerHTML = parseInt(amount.innerHTML) - 1;
+        }
+    }
 }
 
 function addAmount(index, userId, productId) {

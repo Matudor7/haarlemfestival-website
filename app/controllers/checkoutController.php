a9d0135145a90f2c2fa749726551c4ed3acd9e12
Matudor7 <tdr.nsk@gmail.com>
2023-06-19 14:37:43 +0200

    Update checkoutController.php

--- app/controllers/checkoutController.php
+++ app/controllers/checkoutController.php
@@ -9,15 +9,18 @@ require_once __DIR__ . '/../Services/smtpService.php';
 require_once __DIR__ . '/../Services/OrderService.php';
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/../Models/Order.php';
-require_once __DIR__ . '/../Models/ticket.php';
+
 require_once __DIR__ . '/../Services/pdfGenerator.php';
 require_once __DIR__ . '/../Models/productModel.php';
 require_once __DIR__ . '/../Services/productService.php';
+require_once __DIR__ . '/../Services/TicketService.php';
+require_once __DIR__ . '/../Services/QrService.php';
 
 
 
 class CheckoutController extends Controller{
     function index(){
+
         require_once __DIR__ . '/navbarRequirements.php';
         require_once __DIR__ . '/../views/checkout/index.php';
     }
@@ -26,52 +29,19 @@ class CheckoutController extends Controller{
     function payment(){
         require_once __DIR__ . '/../Services/paymentService.php';
         if(isset($_GET["total"]) && isset($_GET["paymentmethod"])){
-            //Ale
             $order = new order();
-            $shoppingCartService = new ShoppingCartService();
-            $shoppingCart = $shoppingCartService->getCartOfUser($_SESSION['user_id']);
-            $tickets = array();
-            $productService = new ProductService();
-            foreach ($shoppingCart->product_id as $item) {
-
-                $product = $productService->getById($item);
-               $ticket = new ticket();
-                switch ($product->getEventType()) {
-                //switch ($item->eventType) {
-                    case 1:
-                        $ticket->setDanceEventId($product->getEventType());
-                        break;
-                    case 2:
-                        $ticket->setYummyEventId($product->getEventType());
-                        break;
-                    case 44:
-                        $ticket->setHistoryEventId($product->getEventType());
-                        break;
-                    case 45:
-                        $ticket->setAccessPassId($product->getEventType());
-                        break;
-                    default:
-                        throw  new Exception("Invalid event type");
-                        break;
-                }
-                $ticket->quantity = 1;
-                $ticket->user_id = $_SESSION['user_id'];
-                //$ticket->setQuantity($item->amount);
-               // $ticket->setUserId($item->user_id);
-                array_push($tickets, $ticket);
-            }
             $order->setInvoiceDate();
             $order->setInvoiceNumber();
             $order->setPaymentId(1);
             $order->setPaymentStatus("Paid");
             $orderService = new OrderService();
             $newOrder = $orderService->insertOrder($order);
-            $this->paymentProcess($newOrder, $tickets);
+            $this->paymentProcess($newOrder);
 
         }
     }
 
-    private function paymentProcess($order, $ticket){
+    private function paymentProcess($order){
         $paymentService = new PaymentService();
         require_once __DIR__ . '/../vendor/autoload.php';
         require_once __DIR__ . '/../Models/Order.php';
@@ -100,7 +70,7 @@ class CheckoutController extends Controller{
             "redirectUrl" => "http://localhost/checkout/return?order_id={$orderId}" ,
             "metadata" => [
                 "order_id" => $orderId,
-                "ticket" => $ticket,
+                //"ticket" => $ticket,
                // "payment_id" => "1", //TODO: change to payment id
                 "userId" => $_SESSION['user_id'],
             ],
@@ -113,9 +83,159 @@ class CheckoutController extends Controller{
         header("Location: ". $payment->getCheckoutUrl());
 
     }
-
-    function return()
+    //function generateTicketPdf($tickets)
+    function generateTicketPdf()
     {
+       // var_dump($this->payment());
+        //Ale
+        $shoppingCartService = new ShoppingCartService();
+        $shoppingCart = $shoppingCartService->getCartOfUser($_SESSION['user_id']);
+        $tickets = array();
+        $productService = new ProductService();
+        $ticketService = new TicketService();
+        //var_dump("SHOPPING CART->product_id");
+
+       // var_dump($shoppingCart->product_id);
+        foreach ($shoppingCart->product_id as $item) {
+           // var_dump("ITEM");
+           // var_dump($item);
+            $product = $productService->getById($item);
+           // var_dump("PRODUCT");
+           // var_dump($product);
+            $ticket = new Ticket();
+
+            switch ($product->getEventType()) {
+                //switch ($item->eventType) {
+                case 1:
+                    $ticket->setDanceEventId($product->getEventType());
+                    break;
+                case 2:
+                    $ticket->setYummyEventId($product->getEventType());
+                    break;
+                case 44:
+                    $ticket->setHistoryEventId($product->getEventType());
+                    break;
+                case 45:
+                    $ticket->setAccessPassId($product->getEventType());
+                    break;
+                default:
+                    throw  new Exception("Invalid event type");
+                    break;
+            }
+            $ticket->quantity = 1;
+            $ticket->user_id = $_SESSION['user_id'];
+            $ticket->setPrice($product->getPrice());
+            //$ticket->setQuantity($item->amount);
+            // $ticket->setUserId($item->user_id);
+            array_push($tickets, $ticketService->storeTicketDB($ticket));
+            //var_dump("TICKETS");
+            //var_dump($tickets);
+
+        }
+        $qrService = new QrService();
+        //get the list of ticket and for each ticket generate a qr code and diplay the rest of the pdf as well with info related to that tickets
+        // $qrService->generateQrCode($ticket);
+        $pdf = "
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset='UTF-8'>
+        <title>Haarlem Festival Ticket</title>
+    </head>
+    <body>
+    <div class='container'>
+        <div class='header'>
+            <h1>Haarlem Festival Ticket</h1> ";
+        foreach ($tickets as $ticket) {
+            $qr_code_ur = $qrService->generateQrCode($ticket);
+            $pdf .= "
+            <div class='qr-code'>
+                <img src='" . $qr_code_ur . "' alt='QR code'>
+            </div>
+    </body>
+    </html>
+<style>
+    body {
+        <!--background-image: url('path/to/background/image.jpg');-->
+        background-size: cover;
+        background-position: center;
+        font-family: Arial, sans-serif;
+        color: #333;
+        margin: 0;
+        padding: 0;
+    }
+
+    .container {
+        width: 100%;
+        max-width: 397px; /* Half of A4 width in pixels */
+        margin: 0 auto;
+        padding: 20px;
+        box-sizing: border-box;
+        background-color: #fff;
+        opacity: 0.9;
+        border: 10px solid #FF6600FF; /* default border color */
+    }
+
+    .container.purple {
+        border-color: #8564CC; /* blue border color for blue event type */
+    }
+
+    .container.blue {
+        border-color: #0d47a1;
+        /*border-color: #3366CFFF;*/ /* green border color for green event type */
+    }
+
+    .header {
+        text-align: center;
+        margin-bottom: 30px;
+    }
+
+    h1 {
+        font-size: 36px;
+        margin: 0;
+        padding: 0;
+    }
+
+    h2 {
+        font-size: 24px;
+        margin: 0;
+        padding: 0;
+    }
+
+    .qr-code {
+        float: right;
+        margin-left: 30px;
+        width: 75px;
+        height: 75px;
+        background-color: #fff;
+        padding: 5px;
+        border: 1px solid #333;
+        border-radius: 3px;
+        text-align: center;
+    }
+
+    .qr-code img {
+        max-width: 100%;
+        height: auto;
+    }
+
+    .info {
+        margin-top: 25px;
+        font-size: 14px;
+    }
+
+    .label {
+        font-weight: bold;
+        display: inline-block;
+        min-width: 70px;
+    }
+</style>";
+        }
+        return $pdf;
+    }
+
+
+    function return(){
         require_once __DIR__ . '/../vendor/autoload.php';
         require_once __DIR__ . '/../Services/paymentService.php';
         $shoppingCartService = new ShoppingCartService();
@@ -123,7 +243,7 @@ class CheckoutController extends Controller{
         //Ale
         require_once __DIR__ . '/../Services/paymentService.php';
         $paymentService = new PaymentService();
-        // $itemsFromShoppingCart = $shoppingCartService->getCartOfUser($_SESSION['user_id']);
+       // $itemsFromShoppingCart = $shoppingCartService->getCartOfUser($_SESSION['user_id']);
         //$paymentDetails = $paymentService->getByPaymentId();
 
         $paymentObject = $paymentService->getByUserId($_SESSION['user_id']);
@@ -131,26 +251,27 @@ class CheckoutController extends Controller{
 
         $mollie = new Mollie\Api\MollieApiClient();
         $mollie->setApiKey('test_mgqJkkMVNtskk2e9vpgsBhUPsTj9K4');
-        $orderService = new OrderService();
+        $orderService =  new OrderService();
         $payment = $mollie->payments->get($paymentObject->getPaymentId());
         $order = $orderService->getOrderById($_GET['order_id'])[0];
 
         if ($payment->isPaid()) {
+            header("Location: http://localhost/");
             //TODO move the logic to the controller
-
+        
 
             $email = $paymentObject->getEmail();
             $fullName = $paymentObject->getFullName();
             $subtotal = "";
-
-            $subject = "Your Haarlem Festival Order Invoice";
-
-            // $message = nl2br("Hello, " . $paymentObject->getFirstName() . ".\n");
+        
+            $subject = "Your Haarlem Festival Order Invoice and Tickets";
+        
+            //$message = nl2br("Hello, " . $paymentObject->getFirstName() . ".\n");
             //$message .= nl2br("Here your order invoice: \n");
 
 
-            $message = "Here is your invoice, thanks for buying your ticket with us!!!";
-            $pdfService = new PDFGenerator();
+                $message = "Here is your invoice and your tickets, thanks for buying your ticket with us!!!";
+                $pdfService = new PDFGenerator();
             if ($payment->isPaid()) {
                 $html = "
         <head>
@@ -170,22 +291,22 @@ class CheckoutController extends Controller{
                     <td style='color:#ff6600;border-top: 1px dashed #ff6600; border-left: none; border-right: none; border-bottom: none; font-weight: bold; text-transform:uppercase;'>BILL TO</td>
                     <td style='color:#ff6600;border-top: 1px dashed #ff6600; border-left: none; border-right: none; border-bottom: none; font-weight: bold;  text-transform:uppercase;'>CUSTOMER'S INF</td>
                     <td style='color:#ff6600; border: none; font-weight: bold; text-transform:uppercase;'>INVOICE#</td>
-                    <td style= 'border-top: 1px dashed #ff6600; border-left: none; border-right: none; border-bottom: none;'>" . $order->invoice_number . "</td>
+                    <td style= 'border-top: 1px dashed #ff6600; border-left: none; border-right: none; border-bottom: none;'>". $order->invoice_number."</td>
                 </tr>
                 <tr>
-                 <td style='border: none;'>" . $paymentObject->first_name . '  ' . $paymentObject->last_name . " </td>
-                    <td style='border: none;'>" . $paymentObject->email . "</td>
+                 <td style='border: none;'>". $paymentObject->first_name . '  '. $paymentObject->last_name." </td>
+                    <td style='border: none;'>".$paymentObject->email."</td>
                     <td style='color: #ff6600; border: none; font-weight: bold; text-transform: uppercase;'>INVOICE DATE</td>
-                    <td style='border: none;'>" . $order->invoice_date . "</td>
+                    <td style='border: none;'>".$order->invoice_date."</td>
                 </tr>
                 <tr>
-                    <td style='border: none;' >" . $paymentObject->address . "</td>
-                    <td style='border: none;'>" . $paymentObject->phone_number . "</td>
+                    <td style='border: none;' >".$paymentObject->address."</td>
+                    <td style='border: none;'>".$paymentObject->phone_number."</td>
                     <td style='color: #ff6600; border: none; font-weight: bold; text-transform: uppercase;'>PAYMENT DATE</td>
-                    <td style='border: none;'>" . $order->invoice_date . "</td>
+                    <td style='border: none;'>".$order->invoice_date."</td>
                 </tr>
                 <tr>
-                    <td style='border: none;' >" . $paymentObject->zip . "</td>
+                    <td style='border: none;' >".$paymentObject->zip."</td>
                 </tr>
             </table>
         </div><br><br>
@@ -204,31 +325,31 @@ class CheckoutController extends Controller{
                 $shoppingCart = $shoppingCartService->getCartOfUser($_SESSION['user_id']);
                 $productService = new ProductService();
                 foreach ($shoppingCart->product_id as $item) {
-                    $product = $productService->getById($item);
-                    $subtotal += intval($shoppingCart->amount[0]) * $product->price;
-                    $totalPricePerProduct = $product->calculateTotalPriceForProduct($shoppingCart->amount[0], $product->price);
-                    $amount = $shoppingCart->amount[0];
-                    $html .= "
-            <tr>
-                <td>" . $product->name . "</td>
-                <td>" . $amount . "</td>
-                <td>&#8364;" . $product->price . "</td>
-                <td>" . $totalPricePerProduct . "</td>
-            </tr> ";
-                }
+                $product = $productService->getById($item);
+                $subtotal += intval($shoppingCart->amount[0]) * $product->price;
+                $totalPricePerProduct = $product->calculateTotalPriceForProduct($shoppingCart->amount[0], $product->price);
+                $amount = $shoppingCart->amount[0];
                 $html .= "
             <tr>
+                <td>".$product->name."</td>
+                <td>".$amount."</td>
+                <td>&#8364;".$product->price."</td>
+                <td>".$totalPricePerProduct."</td>
+            </tr> ";
+            }
+         $html .= "
+            <tr>
                 <td colspan='3'>Subtotal</td>
-                <td>&#8364;" . $subtotal . "</td>
+                <td>&#8364;".$subtotal."</td>
             </tr>
             <tr>
                 <td colspan='3'>Tax</td>
-                <td>&#8364;" . $subtotal * 0.21 . "</td>
+                <td>&#8364;".$subtotal * 0.21."</td>
             </tr>
             <tr>
                 <td colspan='3' style='color:#ff6600;  font-weight: bold;'>TOTAL</td>
         
-                <td style='color:#ff6600;  font-weight: bold;'> &#8364;" . $subtotal * 0.21 + $subtotal . "</td>
+                <td style='color:#ff6600;  font-weight: bold;'> &#8364;".$subtotal * 0.21 + $subtotal."</td>
             </tr>
             </tbody>
         </table>
@@ -287,28 +408,25 @@ class CheckoutController extends Controller{
                 margin: 0;
             }
         </style>";
-                $invoicePdf = $pdfService->createPDF($_GET['order_id'], $_SESSION['user_id'], $html);
+
+                $invoicePdf = $pdfService->createPDF($_GET['order_id'], $_SESSION['user_id'], $html, "invoice");
+                $ticketPDF = $pdfService->createPDF($_GET['order_id'], $_SESSION['user_id'], $this->generateTicketPdf(), "tickets");
                 $smtpService = new smtpService();
-                $smtpService->sendEmail($email, $fullName, $message, $subject, $invoicePdf);
+            $smtpService->sendEmail($email, $fullName, $message, $subject, $invoicePdf, $ticketPDF);
+            //Andy
+            //$this->updateAvailability($itemsFromShoppingCart);
 
-                //Andy
-                $this->updateAvailability($shoppingCart);
-
-                $shoppingCartService->removeCartFromUser($_SESSION['user_id']);
-
-                $smtpService = new smtpService();
-
-                $shoppingCartService->removeCartFromUser($_SESSION['user_id']);
-                return;
-                header("Location: http://localhost/");
-            } else {
-                echo "Payment Failed...";
-            }
-
-            require_once __DIR__ . '/navbarRequirements.php';
-            //  header("Location: http://localhost/");
+            $shoppingCartService->removeCartFromUser($_SESSION['user_id']);
+        } else {
+            echo "Payment Failed...";
         }
     }
+
+
+
+
+
+
     // private function getMolliePayment(){
     //     require_once __DIR__ . '/../vendor/autoload.php';
     //     $mollie = new Mollie\Api\MollieApiClient();
@@ -325,36 +443,14 @@ class CheckoutController extends Controller{
 
     function updateAvailability($shoppingCart){
         require_once __DIR__ . '/../Services/productService.php';
-        require_once __DIR__.'/../Services/shoppingCartService.php';
         $productService = new ProductService();
-        $shoppingCartService = new ShoppingCartService();
 
         $products = $shoppingCart->getProducts();
         $amounts = $shoppingCart->getAmount();
 
         for ($i = 0; $i < count($products); $i++){
             $productService->updateProductAvailability($products[$i], $amounts[$i]);
-            $product = $productService->getById($products[$i]);
-            if($product->getEventType() == 2){
-                $additionalInfo = $shoppingCartService->getAdditionalInfoByProduct($products[$i], $shoppingCart->getUserId());
-                $reservationId = $this->getReservationId($additionalInfo);
-                $this->changeReservationStatus($reservationId);
-            }
         }
     }
-    function getReservationId($additionalNote){
-        $pattern = '/\(R([a-zA-Z0-9]+)\)/';
-        if (preg_match($pattern, $additionalNote, $matches)) {
-            $reservationId = $matches[1];
-        }
-        return 'R'.$reservationId;
-    }
-
-    function changeReservationStatus($reservationId){
-        require_once __DIR__.'/../Services/ReservationService.php';
-        $reservationService = new ReservationService();
-        $reservationStatus = 1;
-        $reservationService->changeReservationStatus($reservationId, $reservationStatus);
-    }
-}
+}}
 ?>

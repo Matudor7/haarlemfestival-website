12bfead5499555e54245f4e32aed3a944345760f
Matudor7 <tdr.nsk@gmail.com>
2023-06-19 14:18:48 +0200

    qr codes can be correctly scanned now

--- app/Services/TicketService.php
+++ app/Services/TicketService.php
@@ -12,8 +12,17 @@ class TicketService
 
     public function getByID($id)
     {
-        $repository = new TicketRepository;
-        $ticket = $repository->getTicketByID();
+        $repository = new TicketRepository();
+        $ticket = $repository->getTicketByID($id);
         return $ticket;
     }
+    public function storeTicketDB($ticket)
+    {
+        $repository = new TicketRepository;
+
+    //var_dump($repository->insert($ticket));
+        return $repository->insert($ticket);
+
+    }
+
 }
